e633e566f08f747a956c7fb25f3316eb44b9c161
Luan Felipe <luan@Mac-mini-de-Luan.local>
2022-08-26 15:05:29 -0400

    Projeto ListaDeContatos finalizado

---
+++ android/app/src/main/kotlin/com/example/udemy_lista_de_contatos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.udemy_lista_de_contatos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:udemy_lista_de_contatos/ui/contact_page.dart';
+import 'package:udemy_lista_de_contatos/ui/home_page.dart';
+
+void main() {
+ runApp(const MaterialApp(
+   home: HomePage(),
+   debugShowCheckedModeBanner: false,
+ ));
+}
